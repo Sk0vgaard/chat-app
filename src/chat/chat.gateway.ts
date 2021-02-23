f082b4c9f00a3193b8a2a904c0f22b953da96e2c
Sk0vgaard <sk0vgaard@live.dk>
2021-02-23 22:07:59 +0100

    Add connecting betweent front and back

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,12 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
 
 @WebSocketGateway()
 export class ChatGateway {
-	@SubscribeMessage('chats')
-	handleChatEvent(@MessageBody() data: string): string {
-		return data + ' hello there';
-	}
+    @WebSocketServer() server;
+    @SubscribeMessage('message')
+    handleChatEvent(@MessageBody() data: string): string {
+        console.log(data);
+        this.server.emit('messages', data);
+        return data + ' Hello';
+    }
 }
