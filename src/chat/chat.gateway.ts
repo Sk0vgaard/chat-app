28031ea5c824dc3deb5686612f269e2db67208d8
Sk0vgaard <sk0vgaard@live.dk>
2021-02-25 18:15:28 +0100

    Show who is connected and disconnected

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,12 +1,27 @@
-import {MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
+import {
+    MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer
+} from '@nestjs/websockets';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server;
     @SubscribeMessage('message')
     handleChatEvent(@MessageBody() data: string): string {
         console.log(data);
         this.server.emit('messages', data);
         return data + ' Hello';
+    }
+
+    public handleConnection(client: any, ...args: any[]): any {
+        console.log('Client connected: ', client.id);
+    }
+
+    public handleDisconnect(client: any): any {
+        console.log('Client disconnected: ', client.id);
     }
 }
